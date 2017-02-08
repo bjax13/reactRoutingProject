7c7e2861b11d2ba029f9df8dc53bffbc4e6354c7
Bryan Jackson <bjack13@gmail.com>
2017-02-08 10:29:31 -0700

    added nav-bar

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className='nav-bar'>
+        <a> Nav <a>
+        <a> store <a>
+      </div>
+    )
+  }
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {Router, Route, hashHistory} from 'react-router';
+import Home from './Home';
 import './index.css';
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
+    <Route path="/" component={Home} />
   </Router>,
   document.getElementById('root')
 );
